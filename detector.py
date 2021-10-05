f67ed22c1683303e83db9e297cb98d34380516f7
Tricky <tricky4724123@gmail.com>
2021-10-05 23:27:11 +0530

    base

---
+++ detector.py
@@ -0,0 +1,8 @@
+import requests
+import re
+import phrases
+
+def find(url):
+    source = str(requests.get(url).content).lower()
+    allphrases = phrases.rickrolls
+    return bool(re.findall("|".join(allphrases), source, re.MULTILINE))
---
+++ main.py
@@ -0,0 +1,6 @@
+from detector import find
+
+url = input("Enter the url to check: ")
+result = find(url)
+
+print(result)
